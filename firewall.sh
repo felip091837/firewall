9431c7df24ac10d53c51c2b6e4e32971ee30cafc
Felipe <felipesampaio@pm.me>
2020-09-11 04:12:37 +0000

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Atribuir IP
-cat <<EOT > /etc/netplan/01-netcfg.yaml
+sudo cat <<EOT > /etc/netplan/01-netcfg.yaml
 network:
   version: 2
   renderer: networkd
@@ -18,7 +18,7 @@ sudo netplan apply
 
 #configurar servidor DNS
 sudo apt install isc-dhcp-server
-cat <<EOT > /etc/netplan/01-netcfg.yaml
+sudo cat <<EOT > /etc/dhcp/dhcpd.conf
 ddns-update-style none;
 
 default-lease-time 600;
@@ -34,7 +34,7 @@ subnet 10.0.0.0 netmask 255.255.255.0 {
 }
 EOT
 
-cat <<EOT > /etc/default/isc-dhcp-server
+sudo cat <<EOT > /etc/default/isc-dhcp-server
 INTERFACESv4="enp0s8"
 INTERFACESv6=""
 EOT
@@ -47,30 +47,29 @@ sudo bash -c 'echo "1" > /proc/sys/net/ipv4/ip_forward'
 sudo iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o enp0s3 -j MASQUERADE
 
 
-
 #REGRAS CLIENTE
 #Definir política de DROP 
-iptables -t filter -P FORWARD DROP
+sudo iptables -t filter -P FORWARD DROP
 
 #permitir respostas das conexões estabelecidas oriundas das regras de permitir tráfego
-iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 #Liberar trafego
-iptables -A FORWARD -p tcp --dport 80 -j ACCEPT
-iptables -A FORWARD -p tcp --dport ssh -j ACCEPT
-iptables -A FORWARD -p udp --dport 53 -j ACCEPT
-iptables -A FORWARD -p icmp -j ACCEPT
-iptables -A FORWARD -p tcp -m string --string "youtube.com" --algo kmp -j DROP
-iptables -A FORWARD -p tcp --dport 21 -j ACCEPT
+sudo iptables -A FORWARD -p tcp --dport 80 -j ACCEPT
+sudo iptables -A FORWARD -p tcp --dport ssh -j ACCEPT
+sudo iptables -A FORWARD -p udp --dport 53 -j ACCEPT
+sudo iptables -A FORWARD -p icmp -j ACCEPT
+sudo iptables -A FORWARD -p tcp -m string --string "youtube.com" --algo kmp -j DROP
+sudo iptables -A FORWARD -p tcp --dport 21 -j ACCEPT
 
 
 
 #REGRAS FIREWALL
 #Definir política de DROP
-iptables -t filter -P INPUT DROP
+sudo iptables -t filter -P INPUT DROP
 
 #permitir respostas das conexões estabelecidas oriundas das regras de permitir tráfego
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 #Liberar SSH
-iptables -A INPUT -p tcp --dport ssh -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport ssh -j ACCEPT
